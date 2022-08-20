938ed237f13f3cca673819f591a30203c9a80011
S-H-E1 <111588540+S-H-E1@users.noreply.github.com>
2022-08-20 19:13:19 +0300

    updated

--- data.js
+++ data.js
@@ -159,7 +159,7 @@ console.log("error 323");
 <table>
 <tbody><tr>
  <td valign="top"><br><br>
-   <form name="index" onsubmit=`` >
+   <form name="index" onsubmit="function(e){e.preventDefault()}" >
      <input type="hidden" name="refresh" value="96222">	
      <table cellspacing="0" cellpadding="1">
         <tbody><tr> 
